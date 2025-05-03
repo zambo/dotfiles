3623ca7efbca61961d1191315940c8f23533a92f
zambo <henrique.zambonin@gmail.com>
2025-05-03 03:05:35 -0300

    feat: proper dotfiles location

---
+++ private_dot_config/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,24 @@
+return {
+  nil,
+}
+
+-- return {
+--   "olimorris/codecompanion.nvim",
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--     "nvim-treesitter/nvim-treesitter",
+--   },
+--   opts = {
+--     strategies = {
+--       -- Change the default chat adapter
+--       chat = {
+--         -- adapter = "anthropic",
+--         adapter = "copilot",
+--       },
+--     },
+--     opts = {
+--       -- Set debug logging
+--       -- log_level = "DEBUG",
+--     },
+--   },
+-- }
