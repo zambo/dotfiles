43257b17a162b8130f0d1054b6d167e57e1bbb15
zambo <henrique.zambonin@gmail.com>
2025-05-03 01:42:50 -0300

    hello world
    git init with current dotfiles

---
+++ dot_dotfiles/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,27 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  event = "BufReadPost",
+  opts = {
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      auto_refresh = true,
+      allow_context_node = false,
+      allow_no_prefix = false,
+      enable_preselect = false,
+      hide_during_completion = vim.g.ai_cmp,
+      keymap = {
+        accept = false, -- handled by nvim-cmp / blink.cmp
+        next = "<M-]>",
+        prev = "<M-[>",
+      },
+    },
+    panel = { enabled = false },
+    filetypes = {
+      markdown = true,
+      help = true,
+    },
+  },
+}
