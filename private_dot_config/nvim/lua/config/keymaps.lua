43fd30a38147ef2a9fd35fe6f92f45f9f93d9c6d
zambo <henrique.zambonin@gmail.com>
2025-06-02 21:13:18 -0300

    feat: update ai tooling

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -10,10 +10,25 @@ util.cowboy()
 map("n", "U", "<C-r>", { desc = "Redo" })
 
 -- Edit inside Word
-vim.keymap.set("n", "<C-c>", "ciw")
+map("n", "<C-c>", "ciw")
 
-vim.keymap.set("n", "<M-Left>", "b", { desc = "Move backwards" })
-vim.keymap.set("n", "<M-Right>", "w", { desc = "Move forwards" })
+map("n", "<M-Left>", "b", { desc = "Move backwards" })
+map("n", "<M-Right>", "w", { desc = "Move forwards" })
+
+-- CodeCompanion
+-- map({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+-- map({ "n", "v" }, "<Leader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+-- map("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+
+-- Expand 'cc' into 'CodeCompanion' in the command line
+vim.cmd([[cab cc CodeCompanion]])
+
+-- Snacks Explorer: Reveal instead of toggle
+-- map("n", "<leader>e", function()
+--   Snacks.explorer.reveal({
+--     hidden = true,
+--   })
+-- end, { desc = "Reveal Explorer" })
 
 -- Disable continuations
 -- map("n", "<Leader>o", "o<Esc>^Da", opts)
--- private_dot_config/nvim/lua/config/lazy.lua
+++ private_dot_config/nvim/lua/config/lazy.lua
@@ -1,4 +1,5 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
@@ -19,6 +20,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
+    { import = "plugins.extras.colorschemes.kanagawa" },
     { import = "plugins" },
   },
   defaults = {
